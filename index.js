94b5af1ebf84e429877aee6fe4bbbe6386c85d7f
wiamouali-star <khayyihanae1@gmail.com>
2025-11-17 19:55:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,10 +8,12 @@ const conversationState = new ConversationState(memoryStorage);
 const selectedNewsProperty = conversationState.createProperty('selectedNews');
 
 // Create adapter
-const adapter = new BotFrameworkAdapter({
-    appId: process.env.MicrosoftAppId || "fda0ef09-2e42-4693-a294-a5408b847009",
-    appPassword: process.env.MicrosoftAppPassword || ""
-});
+// const adapter = new BotFrameworkAdapter({
+//     appId: process.env.MicrosoftAppId || "fda0ef09-2e42-4693-a294-a5408b847009",
+//     appPassword: process.env.MicrosoftAppPassword || ""
+// });
+
+const adapter = new BotFrameworkAdapter({});
 
 // Gestion d'erreur
 adapter.onTurnError = async (context, error) => {
